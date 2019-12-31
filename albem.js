d8b02c3488eab45d4cdd540a963368af0d9dd695
miyazaki.shuhei <51869815+reveal-17@users.noreply.github.com>
2019-12-31 19:44:25 +0900

    Add files via upload

---
+++ albem.js
@@ -0,0 +1,18 @@
+$(function() {
+    function animation() {
+        $('.js-fade-in-img').each(function() {
+            const elemPos = $(this).offset().top;
+            const scroll = $(window).scrollTop();
+            const windowHeight = window.innerHeight;
+            if (scroll > elemPos - windowHeight + 200) {
+                $(this).addClass('js-scroll-img');
+            } else {
+                $(this).removeClass('js-scroll-img');
+            }
+        });
+    }
+
+    $(window).scroll(function() {
+        animation();
+    });
+});
